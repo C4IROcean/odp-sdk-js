cc076629bdca96cc70d3f5f1b1079211ee65448d
Tallak Hellebust <tallak.hellebust@makingwaves.com>
2020-03-20 21:12:18 +0100

    start assets

--- source/asset/index.ts
+++ source/asset/index.ts
@@ -1,3 +1,9 @@
-/**
- * Not done yet
- */
+import { ODPClient } from "../";
+
+export class Assets {
+	private _client: ODPClient;
+
+	constructor(client: ODPClient) {
+		this._client = client;
+	}
+}
