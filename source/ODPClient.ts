4faeba138a3431defb49aa06f492c3c12ef35884
Sindre Seppola <sindre.seppola@oceandata.earth>
2021-11-05 08:45:52 +0100

    fix: (missing ODPClient updates from prev commit)

--- source/ODPClient.ts
+++ source/ODPClient.ts
@@ -1,114 +1,98 @@
-import { CogniteClient } from "@cognite/sdk";
-import { Assets } from "./utils/assets";
-import { Sequences } from "./utils/sequences";
-import { Files } from "./utils/files";
+import { AuthenticationResult, BrowserAuthOptions } from "@azure/msal-browser";
+import { ClientOptions, CogniteClient } from "@cognite/sdk";
+
+import { Auth } from "./auth";
 import { Casts } from "./casts";
-import { MarineRegions } from "./marineRegions";
 
-export interface IClientOptions {
-	/** App identifier (ex: 'FileExtractor') */
-	appId: string;
-	baseUrl?: string;
-}
+// This client id only allows for certain auth_redirects, ideally you'll have a client id per app.
+// Contact us if this is your use case.
+const ODP_SDK_CLIENT_ID = "b2a2d339-e785-4213-a773-8b289abd2199"; // B2C app reg: ODP SDKs
 
-export interface IProject {
-	/**
-	 * Cognite project to login into
-	 */
-	project?: string;
-}
+type RequiredConfig = Pick<ClientOptions, "appId">;
+type OptionalConfig = Partial<Omit<ClientOptions, keyof RequiredConfig | "baseUrl">> &
+	Partial<{
+		// clientId: string;
+		baseUrl: "https://api.cognitedata.com" | "https://westeurope-1.cognitedata.com";
+	}>;
 
-export interface IApiKeyLoginOptions extends IProject {
-	/**
-	 * A Cognite issued api-key
-	 */
-	apiKey: string;
-}
+type AuthListenersT = (token?: AuthenticationResult) => void;
 
-export interface IOAuthLoginOptions extends IProject {
-	onAuthenticate?: any | "REDIRECT" | "POPUP";
-	onTokens?: any;
-	/**
-	 * Provide optional cached access token to skip the authentication flow
-	 * (client.authenticate will still override this).
-	 */
-	accessToken?: string;
-}
+const defaultOptions: Pick<ClientOptions, "project" | "apiKeyMode" | "baseUrl"> = {
+	project: "odp",
+	apiKeyMode: false,
+	baseUrl: "https://api.cognitedata.com",
+};
 
-export default class ODPClient {
-	private _client: CogniteClient = null;
-	// private _timeSeries: TimeSeries;
-	private _sequences: Sequences;
-	private _files: Files;
-	private _assets: Assets;
+export default class ODPClient extends CogniteClient {
+	public authToken: AuthenticationResult | undefined = undefined;
+	private listeners: Array<AuthListenersT> = [];
+
 	private _casts: Casts;
-	private _marineRegions: MarineRegions;
+	private auth: Auth;
+	//  = new Auth(
+	// 	{
+	// 		api: "https://api.cognitedata.com",
+	// 		appClientId: "5c615a6e-62c2-4bca-905e-7bc148a4ade2", // Ocean Data Explorer
+	// 	},
+	// 	{ redirectUri: "https://localhost:3000/oauth_callback" },
+	// );
 
-	public constructor(options: IClientOptions) {
-		this._client = new CogniteClient(options);
+	public constructor(options: RequiredConfig & OptionalConfig, authConfig: BrowserAuthOptions) {
+		super({
+			...defaultOptions,
+			getToken: async () => {
+				const token = await this.auth.handleRedirectAuthV2();
+				if (this.authToken?.uniqueId !== token?.uniqueId) {
+					this.authStateUpdated(token);
+				}
 
-		// this._timeSeries = new TimeSeries(this);
-		this._sequences = new Sequences(this);
-		this._assets = new Assets(this);
-		this._files = new Files(this);
-		this._casts = new Casts(this._sequences);
-		this._marineRegions = new MarineRegions(this._sequences);
+				return token?.accessToken;
+			},
+			...options,
+		});
+
+		this.auth = new Auth(options.baseUrl || defaultOptions.baseUrl, {
+			clientId: ODP_SDK_CLIENT_ID,
+			redirectUri: "http://localhost:3000/", // This should be overwritten!
+			knownAuthorities: ["oceandataplatform.b2clogin.com"],
+			authority:
+				// eslint-disable-next-line max-len
+				"https://oceandataplatform.b2clogin.com/oceandataplatform.onmicrosoft.com/B2C_1A_signup_signin_custom",
+			...authConfig,
+		});
+
+		this._casts = new Casts(this);
 	}
 
-	public get temperatures() {
-		return null;
+	public get unstable() {
+		return {
+			casts: this._casts,
+		};
 	}
-	public get login() {
-		return this._client.login;
-	}
-	public get logout() {
-		return this._client.logout;
-	}
-	public get project() {
-		return this._client.project;
-	}
-	public get cognite() {
-		return this._client;
-	}
-	public loginWithApiKey = (options: IApiKeyLoginOptions) => {
-		if (!options.project) {
-			options.project = "odp";
+
+	public listenToAuthChanges = (listenerFn: AuthListenersT) => {
+		if (!(typeof listenerFn === "function")) {
+			throw new Error(`Listener function is not of type function, got ${typeof listenerFn}`);
 		}
-		this._client.loginWithApiKey(options as any);
+
+		this.listeners.push(listenerFn);
+		return () => {
+			this.listeners = this.listeners.filter((fn) => fn === listenerFn);
+		};
 	};
 
-	public loginWithOAuth = (options?: IOAuthLoginOptions) => {
-		if (!options) {
-			options = {};
+	private notifyListeners = () => {
+		for (const listenerFn of this.listeners) {
+			try {
+				listenerFn(this.authToken);
+			} catch (error) {
+				console.warn("Listener function threw uncaught error", listenerFn);
+			}
 		}
-		if (!options.project) {
-			options.project = "odp";
-		}
-		this._client.loginWithOAuth(options as any);
 	};
 
-	public authenticate = () => this._client.authenticate();
-
-	/*
-	public get timeSeries() {
-		return this._timeSeries;
-	}
-	*/
-
-	public get sequences() {
-		return this._sequences;
-	}
-
-	public get files() {
-		return this._files;
-	}
-	public get asset() {
-		return this._assets;
-	}
-	public get casts() {
-		return this._casts;
-	}
-	public get marineRegions() {
-		return this._marineRegions;
-	}
+	private authStateUpdated = (token?: AuthenticationResult) => {
+		this.authToken = token;
+		this.notifyListeners();
+	};
 }
