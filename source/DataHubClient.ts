0ab8c25966730796b83889b9dfac40416e82b21f
Nikolai Riedel <nikolai.riedel@oceandata.earth>
2022-03-29 16:39:23 +0200

    Add hardcoded data sources and connect search function to it

--- source/DataHubClient.ts
+++ source/DataHubClient.ts
@@ -1,7 +1,37 @@
-import { Auth } from './auth';
+import { Auth } from "./auth";
 
 interface IDataHubClientOptions {
 	auth: Auth;
+}
+
+export interface IMetadata {
+	name: string;
+	source: string;
+	tags: Array<string>;
+	description: string;
+	boundingBox: Array<number>;
+	timeSpan: Array<string>;
+	citation: Array<string>;
+}
+
+enum Filters {
+	Depth = "depth",
+	Time = "time",
+}
+export interface ISearchResult {
+	name: string;
+	description: string;
+	unit?: string;
+	source: string;
+	id: string;
+	sourceUrl: string;
+	tags: Array<string>;
+	dataType: string;
+	filters?: Array<Filters>;
+}
+
+enum DataSources {
+	MapboxVectorTile = "vnd.mapbox-vector-tile",
 }
 
 export default class DataHubClient {
@@ -16,7 +46,15 @@ export default class DataHubClient {
 	}
 
 	public searchFullText = async (type: "DATASET", searchString: string) => {
-		return this._getToken().then((token) => this._searchFullTextWithAuth(type, searchString, token));
+		// In the future we will request this from datahub instead of the hardcoded object.
+		// const token = await this._getToken();
+		// this._searchFullTextWithAuth(type, searchString, token);
+		return hardcodedDataSources.filter(
+			(source) =>
+				source.tags.find((tag) => tag.includes(searchString)) ||
+				source.name.includes(searchString) ||
+				source.id.includes(searchString),
+		);
 	};
 
 	public autocompleteResults = async (searchString: string) => {
@@ -65,12 +103,11 @@ export default class DataHubClient {
 				  }
 				`,
 			}),
-		})
-			.then((r) => r.json());
+		}).then((r) => r.json());
 	};
 
 	private _searchFullTextWithAuth = async (type: "DATASET", searchString: string, token: string) => {
-		return fetch(this._graphQlEndpoint, {
+		fetch(this._graphQlEndpoint, {
 			method: "POST",
 			headers: {
 				authorization: `Bearer ${token}`,
@@ -97,8 +134,7 @@ export default class DataHubClient {
 				}
 			`,
 			}),
-		})
-			.then((r) => r.json());
+		}).then((r) => r.json());
 	};
 
 	private _autocompleteResultsWithAuth = async (searchString: string, token: string) => {
@@ -118,8 +154,7 @@ export default class DataHubClient {
     			}
 			`,
 			}),
-		})
-			.then((r) => r.json());
+		}).then((r) => r.json());
 	};
 
 	private _getTagsWithUrnWithAuth = async (urn: string, token: string) => {
@@ -145,8 +180,7 @@ export default class DataHubClient {
 				  }
 			`,
 			}),
-		})
-			.then((r) => r.json());
+		}).then((r) => r.json());
 	};
 
 	private _getToken = async () => {
@@ -171,3 +205,132 @@ export default class DataHubClient {
 		});
 	};
 }
+
+const hardcodedDataSources: Array<ISearchResult> = [
+	{
+		name: "temperature",
+		description: "this is the temp",
+		source: "mapbox",
+		id: "wod-temperature-year-aggregates",
+		sourceUrl: "mapbox://oceandatafoundation.1nze98kc",
+		tags: ["WOD", "temperature"],
+		dataType: DataSources.MapboxVectorTile,
+		filters: [Filters.Depth],
+		unit: "°C",
+	},
+	{
+		name: "pressure",
+		description: "this is the pressure",
+		source: "mapbox",
+		id: "wod-pressure-aggregates",
+		sourceUrl: "mapbox://oceandatafoundation.pressure_aggregates",
+		tags: ["WOD", "pressure"],
+		dataType: DataSources.MapboxVectorTile,
+		filters: [Filters.Depth, Filters.Time],
+		unit: "N/A",
+	},
+	{
+		name: "oxygen",
+		description: "this is the oxygen",
+		source: "mapbox",
+		id: "wod-oxygen-aggregates",
+		sourceUrl: "mapbox://oceandatafoundation.oxygen_aggregates",
+		tags: ["WOD", "oxygen"],
+		dataType: DataSources.MapboxVectorTile,
+		filters: [Filters.Depth, Filters.Time],
+		unit: "ml/l",
+	},
+	{
+		name: "nitrage",
+		description: "this is the nitrate",
+		source: "mapbox",
+		id: "wod-nitrate-aggregates",
+		sourceUrl: "mapbox://oceandatafoundation.nitrate_aggregates",
+		tags: ["WOD", "nitrate"],
+		dataType: DataSources.MapboxVectorTile,
+		filters: [Filters.Depth, Filters.Time],
+		unit: "µmol/l",
+	},
+	{
+		name: "ph",
+		description: "this is the ph",
+		source: "mapbox",
+		id: "wod-ph-aggregates",
+		sourceUrl: "mapbox://oceandatafoundation.ph_aggregates",
+		tags: ["WOD", "ph"],
+		dataType: DataSources.MapboxVectorTile,
+		filters: [Filters.Depth, Filters.Time],
+		unit: "µmol/l",
+	},
+	{
+		name: "chlorophyll",
+		description: "this is the chlorophyll",
+		source: "mapbox",
+		id: "wod-chlorophyll-aggregates",
+		sourceUrl: "mapbox://oceandatafoundation.chlorophyll_aggregates",
+		tags: ["WOD", "chlorophyll"],
+		dataType: DataSources.MapboxVectorTile,
+		filters: [Filters.Depth, Filters.Time],
+		unit: "N/A",
+	},
+	{
+		name: "alkalinity",
+		description: "this is the alkalinity",
+		source: "mapbox",
+		id: "wod-alkalinity-aggregates",
+		sourceUrl: "mapbox://oceandatafoundation.alkalinity_aggregates",
+		tags: ["WOD", "alkalinity"],
+		dataType: DataSources.MapboxVectorTile,
+		filters: [Filters.Depth, Filters.Time],
+		unit: "N/A",
+	},
+	{
+		name: "phosphate",
+		description: "this is the phosphate",
+		source: "mapbox",
+		id: "wod-phosphate-aggregates",
+		sourceUrl: "mapbox://oceandatafoundation.phosphate_aggregates",
+		tags: ["WOD", "phosphate"],
+		dataType: DataSources.MapboxVectorTile,
+		filters: [Filters.Depth, Filters.Time],
+		unit: "N/A",
+	},
+	{
+		name: "silicate",
+		description: "this is the silicate",
+		source: "mapbox",
+		id: "wod-silicate-aggregates",
+		sourceUrl: "mapbox://oceandatafoundation.silicate_aggregates",
+		tags: ["WOD", "silicate"],
+		dataType: DataSources.MapboxVectorTile,
+		filters: [Filters.Depth, Filters.Time],
+		unit: "N/A",
+	},
+	{
+		name: "windfarms",
+		description: "these are the windfarms",
+		source: "mapbox",
+		id: "norwegian-windfarms",
+		sourceUrl: "mapbox://oceandatafoundation.6nk4oxtx",
+		tags: ["windfarms", "norway"],
+		dataType: DataSources.MapboxVectorTile,
+	},
+	{
+		name: "seacables",
+		description: "these are the seacables",
+		source: "mapbox",
+		id: "norwegian-seacables",
+		sourceUrl: "mapbox://oceandatafoundation.bz79mnos",
+		tags: ["seacables", "norway"],
+		dataType: DataSources.MapboxVectorTile,
+	},
+	{
+		name: "economic zones",
+		description: "these are the economic zones",
+		source: "mapbox",
+		id: "economic-zones",
+		sourceUrl: "mapbox://oceandatafoundation.382xxha1",
+		tags: ["economic zones", "eez"],
+		dataType: DataSources.MapboxVectorTile,
+	},
+];
