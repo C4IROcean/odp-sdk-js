8158c9bdf123190ebfd72a39398f055d89a46e21
Nikolai Riedel <nikolai.riedel@oceandata.earth>
2022-06-02 12:40:22 +0200

    Bump version and switch to data mesh prod version

--- source/constants.ts
+++ source/constants.ts
@@ -97,6 +97,6 @@ export type IMapboxLayerType =
 	| "hillshade"
 	| "background";
 
-export const ODP_DATAMESH_BASE_URL: string = "https://mesh.dev.oceandata.xyz/catalog/v1";
+export const ODP_DATAMESH_BASE_URL: string = "https://mesh.prod.oceandata.xyz/catalog/v1";
 export const ODP_DATAHUB_GRAPHQL_ENDPOINT: string = "https://catalog.dev.oceandata.xyz/api/gms";
 export const ODP_BACKEND_TOKEN_SCOPE: string[] = ["https://oceandataplatform.onmicrosoft.com/odp-backend/ODP_ACCESS"];
